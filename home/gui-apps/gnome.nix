d981868b72fe357a69e05c8d3826417b860d0ccf
Tsiry Mayet <mayet.tsiry@gmail.com>
2026-04-01 21:18:56 +0200

    update

--- home/gui-apps/gnome.nix
+++ home/gui-apps/gnome.nix
@@ -3,6 +3,9 @@
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "appmenu:minimize,maximize,close";
     };
+    "org/gnome/shell/keybindings" = {
+      show-screenshot-ui = [ "Print" "<Super><Shift>s" ];
+    };
     "org/gnome/desktop/background" = {
       picture-uri = "file://${self}/modules/desktops/wallpapers/wallhaven-ex136k.jpg";
       picture-uri-dark = "file://${self}/modules/desktops/wallpapers/wallhaven-ex136k.jpg";
