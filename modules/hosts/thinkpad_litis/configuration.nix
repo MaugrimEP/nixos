195556efbed0d5c92adc4dcb0de020fb9c0b5081
Tsiry Mayet <mayet.tsiry@gmail.com>
2026-04-01 20:32:11 +0200

    update

--- modules/hosts/thinkpad_litis/configuration.nix
+++ modules/hosts/thinkpad_litis/configuration.nix
@@ -46,6 +46,29 @@ services.pipewire = {
   pulse.enable = true;
 };
 
+# Enable bluetooth.
+hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = false;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = true;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+    };
+  };
+};
+
 # Define a user account.
 users.users.tmayet = {
   isNormalUser = true;
