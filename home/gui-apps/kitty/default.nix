7c7b12a917a08f824363db497dde638c218137b8
Tsiry Mayet <mayet.tsiry@gmail.com>
2026-04-03 17:31:58 +0200

    update

--- home/gui-apps/kitty/default.nix
+++ home/gui-apps/kitty/default.nix
@@ -7,6 +7,15 @@
     ];
   };
 
+  xdg.configFile."kitty/launch.conf".source = ./launch.conf;
+
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+    noto-fonts-color-emoji
+    dejavu_fonts
+  ];
+
   home.sessionVariables.TERMINAL = "kitty";
   systemd.user.sessionVariables.TERMINAL = "kitty";
 }
